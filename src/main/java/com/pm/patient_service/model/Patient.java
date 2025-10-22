ddd3fdc9c782531ec9a70d69612d331aaca906c8
shashankBgowda <shashankbg74@gmail.com>
2025-10-22 10:18:56 -0500

    Setup environment, mode-mem-data setup

---
+++ src/main/java/com/pm/patient_service/model/Patient.java
@@ -0,0 +1,81 @@
+package com.pm.patient_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dataOfBirth;
+
+    @NotNull
+    private LocalDate registrationDate;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDataOfBirth() {
+        return dataOfBirth;
+    }
+
+    public void setDataOfBirth(LocalDate dataOfBirth) {
+        this.dataOfBirth = dataOfBirth;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
